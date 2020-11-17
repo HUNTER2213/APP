04740d9681f5068760a8390e74c692f71b4ad0b9
RYNO - X <73104132+HUNTER2213@users.noreply.github.com>
2020-11-17 15:47:49 +0530

    Add files via upload

---
+++ Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUBGMESP
+{
+    internal class Settings
+    {
+        public static bool PlayerESP = true;
+        public static bool PlayerBone = false;
+        public static bool PlayerBox = true;
+        public static bool PlayerLines = false;
+        public static bool PlayerHealth = false;
+        public static bool ItemESP = false;
+        public static bool VehicleESP = false;
+        public static bool ShowMenu = true;
+    }
+}
